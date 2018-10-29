f2ade9abcb280792a9b88147852a7633718ded7e
kevin.trickey <kevin.trickey@ul.com>
2018-10-29 17:20:30 +0000

    Lots of deployment updates

--- K8S/DeployAll.cmd
+++ K8S/DeployAll.cmd
@@ -4,4 +4,15 @@ kubectl apply -f client/%1/ingress.yml
 kubectl apply -f client/%1/service-web.yml
 kubectl apply -f client/%1/web-deployment.yml
 kubectl apply -f client/%1/service-backend.yml
+
+kubectl apply -f client/%1/postcode-pv.yml
+kubectl apply -f client/%1/postcode-pvc.yml
+
 kubectl apply -f client/%1/backend-deployment.yml
+
+kubectl apply -f client/%1/mongo-pv.yml
+kubectl apply -f client/%1/mongo-pvc.yml
+kubectl apply -f client/%1/mongo-deploy.yml
+
+
+
