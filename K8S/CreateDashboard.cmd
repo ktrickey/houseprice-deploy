f091e27e3ed2fe03ee42f6abea1c268237fc10b6
kevin.trickey <kevin.trickey@ul.com>
2018-10-30 16:13:29 +0000

    More deployment stuff

---
+++ K8S/CreateDashboard.cmd
@@ -0,0 +1,1 @@
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
--- K8S/DeployAll.cmd
+++ K8S/DeployAll.cmd
@@ -5,10 +5,11 @@ kubectl apply -f client/%1/service-web.yml
 kubectl apply -f client/%1/web-deployment.yml
 kubectl apply -f client/%1/service-backend.yml
 
-kubectl apply -f client/%1/postcode-pv.yml
+
 kubectl apply -f client/%1/postcode-pvc.yml
 
 kubectl apply -f client/%1/backend-deployment.yml
+
 
 kubectl apply -f client/%1/mongo-pv.yml
 kubectl apply -f client/%1/mongo-pvc.yml
