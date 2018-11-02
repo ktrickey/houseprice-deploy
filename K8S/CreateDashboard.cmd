08162ce6f19b1e31f51fa202ec615624804fa9bf
kevin.trickey <kevin.trickey@ul.com>
2018-11-02 14:43:05 +0000

    Tweaks

--- K8S/CreateDashboard.cmd
+++ K8S/CreateDashboard.cmd
@@ -1,1 +1,5 @@
 kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+
+kubectl create -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/influxdb.yaml
+kubectl create -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/heapster.yaml
+kubectl create -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/grafana.yaml
